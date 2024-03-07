308c5d3c009f7c4a89b2af93b6eb660b157621b8
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2024-03-06 22:14:14 -0500

    toma de fotos en temporal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "pe.edu.idat.appgaleriafotos"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "pe.edu.idat.appgaleriafotos"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding{
+        enable = true
     }
 }
 
